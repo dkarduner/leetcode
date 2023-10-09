5fef246e8b090b6b737aa48995514186500afce0
dkarduner <daniel.karduner@gmail.com>
2023-10-09 16:21:50 +0300

    problem 2651

---
+++ javascript/2651_Calculate_Delayed_Arrival_Time.js
@@ -0,0 +1,17 @@
+// B"H
+// problem in: https://leetcode.com/problems/calculate-delayed-arrival-time/
+
+/**
+ * @param {number} arrivalTime
+ * @param {number} delayedTime
+ * @return {number}
+ */
+
+var findDelayedArrivalTime = function(arrivalTime, delayedTime) {
+    let timeToArrive = arrivalTime + delayedTime;
+    if (timeToArrive >= 24){
+        return timeToArrive % 24;
+    };
+    return timeToArrive;
+};
+
