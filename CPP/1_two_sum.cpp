8f60922ca65d2a5992f7b584194d2865a7b0530f
dkarduner <daniel.karduner@gmail.com>
2023-11-13 16:31:29 +0200

    update folders

---
+++ CPP/1_two_sum.cpp
@@ -0,0 +1,39 @@
+// B"H
+// https://leetcode.com/problems/two-sum/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+//class Solution {
+//public:
+//    vector<int> twoSum(vector<int>& nums, int target) {
+//        // structure to save output
+//        vector<int> output;
+//        int half = output.size();
+//        cout << "test" << " ";
+//
+//        for (int i = 0; i < nums.size(); i++){
+//            cout << nums[i] << " ";
+//
+//            output.insert(output.end(), i);
+//
+//        }
+//        return output;
+//    }
+//};
+
+int main() {
+    vector<int> test = {3, 2, 4};
+    
+    cout << test << endl;
+    //for (const int& i : test) {
+    //    cout << i << endl;
+    //}
+
+    
+
+
+    return 0;
+}
